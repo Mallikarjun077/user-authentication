fa50bcd62d1a7a561d5b5d8281a1bfd44405e2d0
mallikarjun <shirwalmallu@gmail.com>
2025-06-02 16:46:02 +0530

    second

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,8 @@ SECRET_KEY = 'django-insecure-@%tlks=wh06g(rpg*tb+f8e57abv)@8emf^c19fwf_d9pwa_2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['user-authentication-tq4n.onrender.com']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
